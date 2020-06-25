8d09cb44517efcaf5408333af5527ed49b669fb4
Reese <rgrimsle@andrew.cmu.edu>
2020-06-25 16:38:57 -0700

    Functional platform and framework

--- builder/frameworks/commonsense.py
+++ builder/frameworks/commonsense.py
@@ -103,10 +103,6 @@ if "BOARD" in env:
         ]
     )
 
-if ("samd" in build_mcu) or ("samc" in build_mcu):
-    env.Append(
-
-    )
 
 env.Prepend(
     CCFLAGS=[
@@ -126,10 +122,11 @@ env.Prepend(
 env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
 
 
-
+# include a path to the linker script. This will make the binary start at the Reset_Handler function, which calls main after some initialization
 if not board.get("build.ldscript", ""):
-    linker_path = os.path.join(FRAMEWORK_DIR, "linker", "commonsense_linker.ld")
-    env.Replace(LDSCRIPT_PATH=linker_path)
+    # linker_path = os.path.join(FRAMEWORK_DIR, "linker", "commonsense_linker.ld")
+    linker_path = os.path.join(FRAMEWORK_DIR, "linker")
+    env.Replace(LDSCRIPT_PATH="commonsense_linker.ld")
     env.Append(
         LIBPATH=[linker_path]
     )
